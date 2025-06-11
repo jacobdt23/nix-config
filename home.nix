e5d856ced1c4615c427383d1e9889d327318d022
jacobdt23 <turnejac01@gmail.com>
2025-06-11 13:56:34 -0400

    Fix home.nix to correctly reference xorg libraries

--- home.nix
+++ home.nix
@@ -1,6 +1,23 @@
 { config, pkgs, ... }:
 
-{
+let
+  resolve-fhs = pkgs.buildFHSEnv {
+    name = "resolve-fhs";
+    targetPkgs = pkgs: with pkgs; [
+      glib
+      alsa-lib
+      libGL
+      xorg.libX11
+      xorg.libXrandr
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXinerama
+      libuuid
+      ffmpeg
+    ];
+    runScript = "bash ~/.local/bin/resolve.sh";
+  };
+in {
   home.stateVersion = "25.05";
 
   home.username = "nixos";
@@ -11,6 +28,16 @@
     tree
     gh
     vscode
+    resolve-fhs
+    flatpak
+    gimp
+    krita
+    inkscape
+    obs-studio
+    vlc
+    glxinfo
+    vulkan-tools
+    pciutils
   ];
 
   programs.vscode = {
@@ -19,4 +46,15 @@
       github.copilot
     ];
   };
+
+  home.file.".local/bin/resolve.sh" = {
+    text = ''
+      #!/usr/bin/env bash
+      export LIBGL_ALWAYS_INDIRECT=0
+      exec ~/DaVinci_Resolve/Resolve
+    '';
+    executable = true;
+  };
+
+  xdg.enable = true;
 }
